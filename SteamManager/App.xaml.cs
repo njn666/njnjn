0ca7da4efc6db3ed199e2dfccbbcbea7ddfe9ecb
njn666 <114479782+njn666@users.noreply.github.com>
2025-06-13 20:12:35 +0500

    Fix build and add CI

--- SteamManager/App.xaml.cs
+++ SteamManager/App.xaml.cs
@@ -1,8 +1,26 @@
+using System;
 using System.Windows;
 
 namespace SteamManager
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            LoadCulture();
+            var vm = new ViewModels.MainViewModel();
+            vm.LoadAccounts("accounts.json");
+            var window = new Views.MainWindow { DataContext = vm };
+            window.Show();
+        }
+
+        private static void LoadCulture()
+        {
+            var lang = Environment.GetEnvironmentVariable("STEAM_MANAGER_LANG");
+            SteamManager.Resources.Strings.Culture = lang == "ru" ?
+                new System.Globalization.CultureInfo("ru-RU") :
+                new System.Globalization.CultureInfo("en-US");
+        }
     }
 }
