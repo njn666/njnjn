cfc33f94ed82773dce250ac40f4b48a9cff9ccb8
njn666 <114479782+njn666@users.noreply.github.com>
2025-06-13 21:32:31 +0500

    Document STEAM_EXE environment variable

--- SteamManager/GameLauncher.cs
+++ SteamManager/GameLauncher.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Diagnostics;
+using System.IO;
+using System.Windows;
 
 namespace SteamManager
 {
@@ -7,7 +10,29 @@ namespace SteamManager
         public static void LaunchCS2(Account account)
         {
             var args = $"-login {account.Username} {account.Password} -applaunch 730";
-            Process.Start("steam", args);
+
+            var steamPath = Environment.GetEnvironmentVariable("STEAM_EXE");
+            if (string.IsNullOrWhiteSpace(steamPath))
+            {
+                var default1 = @"C:\\Program Files (x86)\\Steam\\steam.exe";
+                var default2 = @"C:\\Program Files\\Steam\\steam.exe";
+                steamPath = File.Exists(default1) ? default1 : default2;
+            }
+
+            try
+            {
+                var psi = new ProcessStartInfo
+                {
+                    FileName = steamPath,
+                    Arguments = args,
+                    UseShellExecute = false
+                };
+                Process.Start(psi);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Failed to launch Steam: {ex.Message}");
+            }
         }
     }
 }
